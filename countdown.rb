5f0a9c4342668bbfb00e424d7e3da4ddf70c096e
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-12 17:16:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while seconds > 0 
+    puts "#{number} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR"
+end
+
+def countdown(number)
+  while seconds > 0 
+    puts "#{number} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR"
 end
