7b6dedc0d163d971083ef8c94628ca48b81a4720
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-12 17:22:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,6 +6,7 @@ def countdown(number)
     number -= 1 
   end
 puts "HAPPY NEW YEAR"
+"HAPPY NEW YEAR"
 end
 
 
@@ -16,4 +17,5 @@ def countdown_with_sleep(number)
     number -= 1 
   end
 puts "HAPPY NEW YEAR"
+"HAPPY NEW YEAR"
 end
