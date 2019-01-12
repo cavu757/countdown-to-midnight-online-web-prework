4ca83d154bf1ea18f59a8ada79bcf2ab8da84af1
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-12 17:43:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,7 @@
 def countdown (number)
   while number > 0 
     puts "#{number} SECOND(S)!"
+    "#{number} SECOND(S)!"
     number -= 1  
   end
 puts "HAPPY NEW YEAR!"
@@ -13,6 +14,7 @@ end
 def countdown_with_sleep (number)
   while number > 0 
     puts "#{number} SECOND(S)!"
+    "#{number} SECOND(S)!"
     sleep 1 
     number -= 1 
   end
